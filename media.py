03732faa33e818458f2dc4371d3a500da9333d51
Michael Rendon <mrr71294@gmail.com>
2018-07-30 15:19:45 -0400

    added comments for Movie() class

--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+# Movie class that creates movie objects with a title, summary,img,and ability to play its movie trailer
 class Movie():
     """Class used to store movie information"""
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
@@ -8,5 +9,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # uses webbrowser to open the objects saved youtube url
     def play_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
